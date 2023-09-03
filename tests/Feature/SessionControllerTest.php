37ed3fe46915bd1126dc8f4b107e963f0de8ecd9
maulanaalfiqih <alfiqiharma@gmail.com>
2023-09-03 21:58:54 +0700

    session

---
+++ tests/Feature/SessionControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class SessionControllerTest extends TestCase
+{
+    public function testCreateSession()
+    {
+        $this->get('/session/create')
+            ->assertSeeText("OK")
+            ->assertSessionHas('userId', 'maulana')
+            ->assertSessionHas('isMember', true);
+    }
+
+    public function testGetSession()
+    {
+        $this->withSession([
+            'userId' => 'maulana',
+            'isMember' => 'true'
+        ])->get('/session/get')
+            ->assertSeeText("User Id = maulana, is member = true");
+    }
+
+    public function testGetSessionFailed()
+    {
+        $this->withSession([])->get('/session/get')
+            ->assertSeeText("User Id = guest, is member = false");
+    }
+}
