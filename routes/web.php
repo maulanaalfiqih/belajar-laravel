ceb9210b763bc92785969ab8cf6e8483409f8e87
maulanaalfiqih <alfiqiharma@gmail.com>
2023-09-04 01:33:32 +0700

    error handling

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Exceptions\ValidationException;
 use App\Http\Controllers\CookieController;
 use App\Http\Controllers\FileController;
 use App\Http\Controllers\FormController;
@@ -148,3 +149,16 @@ Route::get('/url/current', function () {
 
 Route::get('/session/create', [SessionController::class, 'createSession']);
 Route::get('/session/get', [SessionController::class, 'getSession']);
+
+Route::get('/error/sample', function () {
+    throw new Exception('Sample Error');
+});
+
+Route::get('/error/manual', function () {
+    report(new Exception('Sample Error'));
+    return "OK";
+});
+
+Route::get('/error/validation', function () {
+    throw new ValidationException('Validation Error');
+});
